819e4e81f69683bbb955be3af1776952346ec251
followin <dlike.version10@gmail.com>
2023-08-01 01:48:51 +0300

    Migration to nixos

---
+++ configuration.nix
@@ -0,0 +1,188 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ inputs, config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.resumeDevice = "/dev/disk/by-label/swap";
+
+  nix.settings = {
+    substituters = [ 
+      "https://cache.nixos.org" 
+      "https://nixcache.reflex-frp.org" 
+      "https://hyprland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Kyiv";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "JetBrainsMono Nerd Font";
+    useXkbConfig = true; # use xkbOptions in tty.
+  };
+
+  # # Enable the X11 windowing system.
+  environment.pathsToLink = [ "/libexec" ];
+  services.xserver = {
+    enable = true;
+    
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    displayManager = {
+      defaultSession = "none+i3";
+      autoLogin.enable = true;
+      autoLogin.user = "main";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3lock
+        i3blocks
+      ];
+    };
+    
+    libinput.enable = true; # touchpad
+    libinput.touchpad.naturalScrolling = true;
+    libinput.touchpad.disableWhileTyping = true;
+    libinput.touchpad.additionalOptions = ''
+      Option "ScrollPixelDistance" "50"
+    '';
+
+    layout = "us,ru";
+    xkbOptions = "ctrl:nocaps,grp:caps_shift_toggle,grp:shift_caps_toggle";
+  };
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  nixpkgs.config.allowUnfree = true;
+  programs.fish.enable = true;
+  users.users.main = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "audio" "input" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      nodejs_20
+
+      dotnet-sdk_7
+      dotnet-sdk_8
+
+      rustup
+
+      luajitPackages.luarocks
+
+      nodePackages.eslint
+
+      google-chrome
+      discord
+      telegram-desktop
+      zoom-us
+    ];
+  };
+  users.defaultUserShell = pkgs.fish;
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+  ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+
+  networking.networkmanager.enable = true;
+  environment.systemPackages = with pkgs; [
+    neovim
+    wget
+    curl
+    networkmanager
+    pavucontrol
+    git
+    kitty
+    fzf
+    bat
+    fd
+    unzip
+    binutils
+    gcc
+    glibc
+    sysstat
+    acpi
+    btop
+    xclip
+    libinput-gestures
+  ];
+
+  environment.variables = rec {
+    TERMINAL = "kitty";
+    EDITOR = "nvim";
+  };
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
+
---
+++ home.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "main";
+  home.homeDirectory = "/home/main";
+
+  home.stateVersion = "23.05";
+
+  home.file.".tmux.conf".source = ./home/.tmux.conf;
+
+  home.file.".config/fish/user" = {
+    source = ./home/.config/fish/user;
+    recursive = true;
+  };
+  home.file.".config/fish/config.fish".source = ./home/.config/fish/config.fish;
+  home.file.".config/fish/fish_plugins".source = ./home/.config/fish/fish_plugins;
+
+  home.file.".config/nvim/lua/user/init.lua".source = ./home/.config/nvim.lua;
+
+  home.file.".config/i3/config".source = ./home/.config/i3config;
+
+  home.file.".config/libinput-gestures.conf".source = ./home/.config/libinput-gestures.conf;
+
+
+  programs.git = {
+    enable = true;
+    userName = "followin";
+    userEmail = "dlike.version10@gmail.com";
+  };
+
+  programs.home-manager.enable = true;
+}
