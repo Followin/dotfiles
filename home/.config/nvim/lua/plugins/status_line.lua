7ec895eafc8414ca337210f2f5b77c7e64455fe1
followin <dlike.version10@gmail.com>
2023-08-24 18:12:42 +0300

    Add lsp info to status bar

--- home/.config/nvim/lua/plugins/status_line.lua
+++ home/.config/nvim/lua/plugins/status_line.lua
@@ -1,3 +1,52 @@
 return {
-  { 'nvim-lualine/lualine.nvim', opts = { } },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'linrongbin16/lsp-progress.nvim',
+    },
+    config = function()
+      require("lualine").setup({
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "filename" },
+          lualine_c = {
+              -- invoke `progress` here.
+            [[
+              require("lsp-progress").progress({
+                format = function(messages)
+                    local active_clients = vim.lsp.get_active_clients()
+                    local client_count = #active_clients
+                    if #active_clients <= 0 then
+                        return ""
+                    else
+                        local client_names = {}
+                        for i, client in ipairs(active_clients) do
+                            if client and client.name ~= "" then
+                                table.insert(client_names, client.name)
+                            end
+                        end
+                        return " "
+                            .. table.concat(client_names, " ")
+                    end
+                end,
+              })
+            ]]
+          },
+        }
+      })
+
+      -- listen lsp-progress event and refresh lualine
+      vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+      vim.api.nvim_create_autocmd("User LspProgressStatusUpdated", {
+        group = "lualine_augroup",
+        callback = require("lualine").refresh,
+      })
+    end,
+  },
+  {
+    'linrongbin16/lsp-progress.nvim',
+    config = function()
+      require('lsp-progress').setup()
+    end
+  }
 }
