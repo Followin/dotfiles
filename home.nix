9ced46791b52010a9d54eeda3f357650ea61dd76
Followin <dlike.version10@gmail.com>
2025-03-31 12:16:04 +0200

    Update

--- home.nix
+++ home.nix
@@ -3,7 +3,7 @@
 let
   dotnetPkg = (with pkgs-unstable.dotnetCorePackages; combinePackages [
     sdk_8_0
-    sdk_9_0
+    # sdk_9_0
   ]);
   omnisharp =
     (pkgs-unstable.writeShellScriptBin "omnisharp" ''
